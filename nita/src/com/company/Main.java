14ffa70983b49210817ce887270baf4941a7c833
alvianita kusuma <37771180+alvianita@users.noreply.github.com>
2018-03-28 11:19:19 +0700

    Update Main.java

--- nita/src/com/company/Main.java
+++ nita/src/com/company/Main.java
@@ -7,19 +7,24 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Scanner show = new Scanner(System.in);
-        System.out.print("Masukan Jumlah Deret Fibonacci : ");
+        System.out.print("Input : ");
         int n = show.nextInt();
         long fib[] = new long[n];
 
-        fib[0] = 0;
+        long temp = 0;
+
+        fib[0] = 1;
         fib[1] = 1;
 
+        System.out.println("");
+        System.out.println("Output");
         for(int i = 2; i < n; i++) {
             fib[i] = fib[i-1] + fib[i-2];
         }
 
         for (int i = 0; i < n; i++) {
-            System.out.print(fib[i] +  " ");
+            temp = temp + fib[i];
         }
+        System.out.println(temp);
     }
 }
